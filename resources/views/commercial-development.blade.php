c286c2454944bc3ec40a6c27a1442d74f68e41e7
Oscar López <Oscar@Oscar.local>
2020-11-02 18:47:33 -0600

    Arquitectura: Diseño de interiores y Desarrollo Comercial

--- resources/views/commercial-development.blade.php
+++ resources/views/commercial-development.blade.php
@@ -21,17 +21,53 @@
 
 	<div class="container clearfix">
 
-		<div class="title-block">
-			<h2>
-				<div class="text-rotater" data-separator="|" data-rotate="bounceIn" data-speed="2500">
-					Sabemos lo importante que es tu negocio, por lo que nos preocupamos y nos ocupamos por que funcione correctamente.
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="title-block">
+					<h2>
+						<div class="text-rotater" data-separator="|" data-rotate="bounceIn" data-speed="2500">
+							Sabemos lo importante que es tu negocio, por lo que nos preocupamos y nos ocupamos por que funcione correctamente.
+						</div>
+					</h2>
+					<span>¿Cómo? Mediante un proyecto pensado desde un diseño funcional, original, con personalidad, una correcta y óptima operación, hasta el impacto que va a generar al cliente.</span>
 				</div>
-			</h2>
-			<span>¿Cómo? Mediante un proyecto pensado desde un diseño funcional, original, con personalidad, una correcta y óptima operación, hasta el impacto que va a generar al cliente.</span>
-		</div>
 
-		<p>Esto mediante un análisis de las necesidades primeramente de ti que vas a brindar un servicio, ¿Qué? ¿Cómo? Tanto del cliente para poder ofrecerle un servicio excepcional. No importa si es un espacio pequeño, mediano o grande, lo importante es transmitir al cliente la seguridad de que están en un lugar especial.</p>
-		<p>Esto también puede ser implementado con la tecnología necesaria para que puedas mantener el control de tu negocio a través de control manual o de voz.</p>
+				<p>Esto mediante un análisis de las necesidades primeramente de ti que vas a brindar un servicio, ¿Qué? ¿Cómo? Tanto del cliente para poder ofrecerle un servicio excepcional. No importa si es un espacio pequeño, mediano o grande, lo importante es transmitir al cliente la seguridad de que están en un lugar especial.</p>
+				<p>Esto también puede ser implementado con la tecnología necesaria para que puedas mantener el control de tu negocio a través de control manual o de voz.</p>
+			</div>
+			<div class="col-lg-6">
+				 <div class="fslider mb-5" data-easing="easeInQuad">
+					<div class="flexslider">
+						<div class="slider-wrap">
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/2.jpg">
+								<a href="#">
+									<img src="{{ asset('images/architecture/commercial-1.jpg') }}" alt="@yield('title')">
+									<div class="flex-caption slider-caption-bg">@yield('title')</div>
+								</a>
+							</div>
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/3.jpg">
+								<a href="#">
+									<img src="{{ asset('images/architecture/commercial-2.jpg') }}" alt="@yield('title')">
+									<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-top-left">@yield('title')</div>
+								</a>
+							</div><!-- 
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/4.jpg">
+								<a href="#">
+									<img src="images/slider/boxed/4.jpg" alt="Slide 4">
+									<div class="flex-caption slider-caption-bg slider-caption-top-right">Bootstrap 3+ Compatible</div>
+								</a>
+							</div>
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/5.jpg">
+								<a href="#">
+									<img src="images/slider/boxed/5.jpg" alt="Slide 5">
+									<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">eCommerce Design Included</div>
+								</a>
+							</div> -->
+						</div>
+					</div><!--.flexslider-->
+				</div><!--.fslider-->
+			</div>
+		</div><!--.row-->
 
 	</div><!--.container-->
 	
--- resources/views/interior-design.blade.php
+++ resources/views/interior-design.blade.php
@@ -20,17 +20,53 @@
 
 	<div class="container clearfix">
 
-		<div class="title-block">
-			<h2>
-				<div class="text-rotater" data-separator="|" data-rotate="bounceIn" data-speed="2500">
-					 Nos involucramos en el proceso de mejorar la función y cualidades del espacio interior mediante la manipulación de los <span class="t-rotate color">materiales|texturas|objetos|colores</span>, etc.
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="title-block">
+					<h2>
+						<div class="text-rotater" data-separator="|" data-rotate="bounceIn" data-speed="2500">
+							 Nos involucramos en el proceso de mejorar la función y cualidades del espacio interior mediante la manipulación de los <span class="t-rotate color">materiales|texturas|objetos|colores</span>, etc.
+						</div>
+					</h2>
+					<span>Esto, con el fin de construir un ambiente confortable que mejore la calidad de vida y cultura de los ocupantes y sea estéticamente atractivo.</span>
 				</div>
-			</h2>
-			<span>Esto, con el fin de construir un ambiente confortable que mejore la calidad de vida y cultura de los ocupantes y sea estéticamente atractivo.</span>
-		</div>
 
-		<p>Cada mobiliario, cada color, cada textura, cada elemento decorativo e iluminación son de suma importancia para poder lograr la ambientación esperada.</p>
-		<p>Nuestro proceso de diseño interior sigue una metodología sistemática y coordinada, incluida la investigación, integración y análisis del conocimiento en el proceso creativo, en el que las necesidades y recursos del cliente son satisfechas para producir un espacio que cumpla con los objetivos del proyecto.</p>
+				<p>Cada mobiliario, cada color, cada textura, cada elemento decorativo e iluminación son de suma importancia para poder lograr la ambientación esperada.</p>
+				<p>Nuestro proceso de diseño interior sigue una metodología sistemática y coordinada, incluida la investigación, integración y análisis del conocimiento en el proceso creativo, en el que las necesidades y recursos del cliente son satisfechas para producir un espacio que cumpla con los objetivos del proyecto.</p>
+			</div>
+			<div class="col-lg-6">
+				<div class="fslider mb-5" data-easing="easeInQuad">
+					<div class="flexslider">
+						<div class="slider-wrap">
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/2.jpg">
+								<a href="#">
+									<img src="{{ asset('images/architecture/interior-design-1.jpg') }}" alt="@yield('title')">
+									<div class="flex-caption slider-caption-bg">@yield('title') 1</div>
+								</a>
+							</div>
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/3.jpg">
+								<a href="#">
+									<img src="{{ asset('images/architecture/interior-design-2.jpg') }}" alt="@yield('title')">
+									<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-top-left">@yield('title')</div>
+								</a>
+							</div><!-- 
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/4.jpg">
+								<a href="#">
+									<img src="images/slider/boxed/4.jpg" alt="Slide 4">
+									<div class="flex-caption slider-caption-bg slider-caption-top-right">Bootstrap 3+ Compatible</div>
+								</a>
+							</div>
+							<div class="slide" data-thumb="images/slider/boxed/thumbs/5.jpg">
+								<a href="#">
+									<img src="images/slider/boxed/5.jpg" alt="Slide 5">
+									<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">eCommerce Design Included</div>
+								</a>
+							</div> -->
+						</div>
+					</div><!--.flexslider-->
+				</div><!--.fslider-->
+			</div>
+		</div><!--.row-->
 
 	</div><!--.container-->
 	
